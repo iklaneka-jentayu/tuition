e0d5e527a29db5b817894b9ecfd3414f9f8c6638
iklaneka-jentayu <iklaneka.jentayu@gmail.com>
2026-02-02 17:19:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Initialize form validation
 if (document.readyState !== 'loading') {
     console.log('document is already ready, just execute code here');
+    alert('document is already ready, just execute code here');
     myInitCode();
 } else {
     document.addEventListener('DOMContentLoaded', function () {
         console.log('document was not ready, place code here');
+        alert('document was not ready, place code here');
         myInitCode();
     });
 }
